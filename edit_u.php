b91aaa42d3986b5b7e430e33366191f01640d1bd
ntossapo <ntossapo@gmail.com>
2014-11-11 21:06:53 +0700

    add new file edit_user.php that can edit user

---
+++ edit_u.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Fineminds Marketing Solutions Website Template</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div class="border">
+		<div id="bg">
+			background
+		</div>
+		<div class="page">
+			<div class="sidebar">
+				<a href="index.html" id="logo"><img src="images/logo223.png" alt="logo"></a>
+				<ul>
+					<li class="selected">
+					<a href="admin_page.php">Home</a></li>
+					<li><a href="view.php">GET Router</a>
+				  </li>
+					<li>
+						<a href="view_page.php">Manager USER</a>
+					</li>
+					<li>
+						<a href="logout.php">LOgout</a>
+					</li>
+					<li></li>
+				</ul>
+				<form action="about.html">
+					<span>Newsletter Signup</span>
+					<input type="text" onClick="this.value='';" onFocus="this.select()" onBlur="this.value=!this.value?'Enter Valid Email Address':this.value;" value="Enter Valid Email Address">
+					<input type="submit" id="submit" value="submit">
+			  </form>
+				<div class="connect">
+					<a href="http://freewebsitetemplates.com/go/facebook/" id="facebook">facebook</a> <a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a> <a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">youtube</a>
+				</div>
+				<p>
+					Copyright 2023
+				</p>
+				<p>
+					Fineminds Marketing Solutions
+				</p>
+		  </div>
+			<div class="body">
+				<div>
+					<h2>Welcome to Admin Page! </h2>
+					<p>&nbsp;</p>
+					<div>
+					  <h3>&nbsp;</h3><center>
+					  <form name="form1" method="post" action="add_u.php">
+  Add Router<br>
+  <table width="478" border="1" style="width: 600px">
+    <tbody>  <tr>
+        <td>&nbsp;User_ID</td>
+        <td><input type="text" name="User_ID" id="User_ID"></td>
+      </tr>
+      <tr>
+        <td width="157"> &nbsp;Username</td>
+        <td width="305"><input type="text" name="Username" id="Username"></td>
+      </tr>
+       <tr>
+        <td width="157"> &nbsp;Name</td>
+        <td width="305"><input type="text" name="Name" id="Name"></td>
+      </tr>
+       <tr>
+        <td width="157"> &nbsp;Status</td>
+        <td width="305"><input type="text" name="Status" id="Status"></td>
+      </tr>
+	   </tr>
+       <tr>
+        <td width="157"> &nbsp;Password</td>
+        <td width="305"><input type="text" name="Password" id="Password"></td>
+      </tr>
+   
+    </tbody>
+  </table>
+  <br>
+  <input type="submit" name="Submit" value="Save">
+</form>
+
+                      </center>
+						<p>&nbsp;</p>
+                  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
